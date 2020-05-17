ffd9e6ba0638c5071b1648331f546ec957a9b05b
Dawid Rusin <dawidrusin90@gmail.com>
2020-05-17 18:35:30 +0200

    exported to html

--- dodge-the-creeps/Mob.js
+++ dodge-the-creeps/Mob.js
@@ -5,9 +5,10 @@ export default class Mob extends godot.RigidBody2D {
 		godot.register_property(Mob, 'minSpeed', 150);
 		godot.register_property(Mob, 'maxSpeed', 250);
 	}
-
+	
 	_ready() {
 		this.get_node('AnimatedSprite').animation = MOB_TYPES[godot.abs(godot.randi() % MOB_TYPES.length)];
+		this.get_node('AnimatedSprite').play();
 	}
 
 	_onVisibilityScreenExited() {
